642af4185bb490690c84fa25c213346978cb6257
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-18 01:02:13 +0500

    All Activities are now clickable

--- app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
+++ app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bgChecked = (LinearLayout) findViewById(R.id.checked);
+        bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
         bgChecked.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "New Arrival Activity has been clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
