7d1df4e2185e33e1db492342d18fe56b85abe843
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-18 01:08:41 +0500

    All Activities are now clickable

--- app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
+++ app/src/main/java/mrperfect/example/discoverapp/MainActivity.java
@@ -11,11 +11,60 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout bgChecked;
+    LinearLayout bglink;
+    LinearLayout bghome;
+    LinearLayout bgFilm;
+    LinearLayout bgAdd;
+    LinearLayout bgDownloads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
+        bgChecked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "New Arrival Activity has been clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
+        bgChecked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "New Arrival Activity has been clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
+        bgChecked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "New Arrival Activity has been clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
+        bgChecked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "New Arrival Activity has been clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         bgChecked = (LinearLayout) findViewById(R.id.bg_checked);
         bgChecked.setOnClickListener(new View.OnClickListener() {
